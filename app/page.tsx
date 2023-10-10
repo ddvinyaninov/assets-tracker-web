ca10df8b55e7414c6b48cb79566613bf233ea967
Dima <ddvinyaninov@gmail.com>
2023-10-09 22:59:32 -0700

    Polish map boundaries

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Cluster from '../components/cluster';
 export default function IndexPage() {
     return (
         <main className="p-4 md:p-10 mx-auto max-w-7xl">
-            <Card className="mt-6" style={{'width': 860}}>
+            <Card className="mt-6" style={{'width': 850}}>
                 <Cluster/>
             </Card>
         </main>
